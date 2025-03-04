119f2b29d51e4d1cf2f7dd45a2145cf13c2fea1e
pcwang <gg21aping@gmail.com>
2025-03-04 20:52:23 +0000

    Implement queue tail removal operation
    Implement function to remove elements from the tail of queue. The
    functions work alike when removing either the head or the tail element
    from the queue, and thus can be improved.
    
    Change-Id: Ic24300ffc6782ff31384f588303cd3370296ad06

--- queue.c
+++ queue.c
@@ -91,7 +91,21 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    element_t *e;
+
+    if (!head || list_empty(head))
+        return NULL;
+
+    e = list_last_entry(head, element_t, list);
+
+    list_del(&e->list);
+
+    if (sp && e->value) {
+        strncpy(sp, e->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return e;
 }
 
 /* Return number of elements in queue */
